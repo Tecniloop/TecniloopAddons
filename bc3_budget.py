a61e9500ca88dfbdd767ca6473415e8783876d4c
Tecniloop <96119594+Tecniloop@users.noreply.github.com>
2026-06-26 16:54:39 +0200

    Add files via upload

---
+++ bc3_budget.py
@@ -0,0 +1,82 @@
+# Translation of Odoo Server.
+# This file contains the translation of the following modules:
+#	* tl_bc3_budget
+#
+msgid ""
+msgstr ""
+"Project-Id-Version: Odoo Server 19.0\n"
+"Language: es\n"
+"MIME-Version: 1.0\n"
+"Content-Type: text/plain; charset=UTF-8\n"
+"Content-Transfer-Encoding: 8bit\n"
+
+#. module: tl_bc3_budget
+#: code:addons/tl_bc3_budget/:0
+msgid "BC3 Budget Lines"
+msgstr "Líneas de presupuesto BC3"
+
+#. module: tl_bc3_budget
+#: code:addons/tl_bc3_budget/:0
+msgid "BC3 Budgets"
+msgstr "Presupuestos BC3"
+
+#. module: tl_bc3_budget
+#: code:addons/tl_bc3_budget/:0
+msgid "Budget Lines"
+msgstr "Líneas de presupuesto"
+
+#. module: tl_bc3_budget
+#: code:addons/tl_bc3_budget/:0
+msgid "Budgets"
+msgstr "Presupuestos"
+
+#. module: tl_bc3_budget
+#: code:addons/tl_bc3_budget/:0
+msgid "Build Lines"
+msgstr "Construir líneas"
+
+#. module: tl_bc3_budget
+#: code:addons/tl_bc3_budget/:0
+msgid "Chapter"
+msgstr "Capítulo"
+
+#. module: tl_bc3_budget
+#: code:addons/tl_bc3_budget/:0
+msgid "Percentage"
+msgstr "Porcentaje"
+
+#. module: tl_bc3_budget
+#: code:addons/tl_bc3_budget/:0
+msgid "Quantity"
+msgstr "Cantidad"
+
+#. module: tl_bc3_budget
+#: code:addons/tl_bc3_budget/:0
+msgid "Rebuild Lines"
+msgstr "Reconstruir líneas"
+
+#. module: tl_bc3_budget
+#: code:addons/tl_bc3_budget/:0
+msgid "Resource"
+msgstr "Recurso"
+
+#. module: tl_bc3_budget
+#: code:addons/tl_bc3_budget/:0
+msgid "Root"
+msgstr "Raíz"
+
+#. module: tl_bc3_budget
+#: code:addons/tl_bc3_budget/:0
+msgid "Subtotal"
+msgstr "Subtotal"
+
+#. module: tl_bc3_budget
+#: code:addons/tl_bc3_budget/:0
+msgid "Unit Price"
+msgstr "Precio unitario"
+
+#. module: tl_bc3_budget
+#: code:addons/tl_bc3_budget/:0
+msgid "Work Unit"
+msgstr "Partida"
+
