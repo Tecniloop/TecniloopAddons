d5a5665bde05b13ce1fc63e39d19b4a3b34494de
Tecniloop <96119594+Tecniloop@users.noreply.github.com>
2026-08-02 11:09:01 +0200

    Add files via upload

--- tl_sitemap_product_import/__manifest__.py
+++ tl_sitemap_product_import/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Importación de productos por sitemap',
-    'version': '19.0.1.122.0',
+    'version': '19.0.1.125.0',
     'category': 'Sales/Sales',
     'summary': 'Importa y actualiza productos desde el sitemap público de una o varias tiendas online',
     'description': """
